0a927360f48dc570d91bcfc1e0912040a1164367
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-11-26 23:47:37 +0000

    displayed a name in the child from the parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'input-output-decorator';
+  public name = "deepa"
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-test',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit {
+  //Hey there is a data coming from the parent so should be notified in @Input()
+  @Input() public parentData;
 
   constructor() { }
 
