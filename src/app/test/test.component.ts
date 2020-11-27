873f9930236bc34aca88d71f517205605f69e0c2
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-11-27 00:11:49 +0000

    created a custom event

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-test',
@@ -7,9 +7,17 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TestComponent implements OnInit {
   // 1.Hey there is a data coming from the parent so should be notified in @Input()
+
   // @Input() public parentData;
-  //2.another method of input using alias hey this an input but in this component I want to refered this as name
+
+  //2.another method of input using alias hey this an input but in this component
+  //  I want to refered this as name
+
   @Input('parentData') public name;
+
+  //sending data from child to parent.
+  //create an instance of the event emitter class and add output decorator
+  @Output() public childEvent = new EventEmitter()
 
   constructor() { }
 
